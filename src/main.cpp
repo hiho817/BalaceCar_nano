ca2f8e4a4439002c2d14f5e9168b34af4ace336a
hiho817 <kenny0817123@gmail.com>
2024-08-15 14:19:59 +0800

    add the init func

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <Arduino.h>
+bool initIMU(){
+
+}
+bool initBT(){
+
+}
+
 
 
 void setup() {
+  Serial.begin(115200);
+  if(!initIMU()){
+    Serial.println("intialize IMU sucessful");
+  }
+  if(!initBT()){
+    Serial.println("intialize BlueTooth sucessful");
+  }
+
 }
 
 void loop() {
