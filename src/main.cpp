fc03c5908b021d7025503a30301abb0edbbcb970
hiho817 <kenny0817123@gmail.com>
2024-08-15 14:44:53 +0800

    define pinout on nano

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,24 @@
 #include <Arduino.h>
+
+//****** pinout define ******//
+
+//          ||D13    typeC    D12||
+//          ||3V3             D11||
+//          ||REF             D10||
+//          ||A0               D9||
+//          ||A1               D8||DIR_R
+//          ||A2               D7||DIR_L
+//          ||A3               D6||STEP_R
+//   IMU_SDA||A4               D5||STEP_L
+//   IMU_SCL||A5               D4||
+//          ||A6               D3||
+//          ||A7               D2||
+//          ||5V              GND||
+//          ||RST             RST||
+//          ||GND              RX||BT_TX
+//          ||VIN              TX||BT_RX
+
+
 bool initIMU(){
 
 }
